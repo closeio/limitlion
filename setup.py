6ceb5cdd0772ef29da229fc4894ac080e85c2fc1
Joe Kemp <joe@close.io>
2018-06-13 15:07:33 -0400

    Add more info

--- setup.py
+++ setup.py
@@ -17,11 +17,14 @@ setup(
     description='Close.io LimitLion',
     platforms='any',
     classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     packages=[
         'limitlion',
