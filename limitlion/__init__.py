de1461de0947bfe1bb24e5179bf1e98949607385
Joe Kemp <joe@close.io>
2020-10-06 16:18:29 -0400

    Export RunningCounter

--- limitlion/__init__.py
+++ limitlion/__init__.py
@@ -1,15 +1,18 @@
 """LimitLion package."""
 
 from .throttle import *
+from .running_counter import RunningCounter
 
-__all__ = ['throttle',
-           'throttle_configure',
-           'throttle_delete',
-           'throttle_get',
-           'throttle_reset',
-           'throttle_set',
-           'throttle_wait',
-           'THROTTLE_BURST_DEFAULT',
-           'THROTTLE_WINDOW_DEFAULT',
-           'THROTTLE_REQUESTED_TOKENS_DEFAULT',
-           ]
+__all__ = [
+    'RunningCounter',
+    'throttle',
+    'throttle_configure',
+    'throttle_delete',
+    'throttle_get',
+    'throttle_reset',
+    'throttle_set',
+    'throttle_wait',
+    'THROTTLE_BURST_DEFAULT',
+    'THROTTLE_WINDOW_DEFAULT',
+    'THROTTLE_REQUESTED_TOKENS_DEFAULT',
+]
