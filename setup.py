c1f40d8ec367c6b0324f37fe3e7760d715d5209e
Alec Rosenbaum <alec.rosenbaum@close.com>
2025-06-19 08:28:24 -0400

    update trove classifiers

--- setup.py
+++ setup.py
@@ -22,8 +22,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     packages=[
